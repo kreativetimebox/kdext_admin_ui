fddec56d3d80bf2fef3e97b0d2896a5964690281
VanshTomar <vansh.tomar.1303@gmail.com>
2026-04-18 12:48:40 +0530

    Update page.js

--- app/missing-fields/page.js
+++ app/missing-fields/page.js
@@ -132,9 +132,6 @@ function getMissingFieldKeys(rawOcrUiResults, docType = "") {
   }
 
   if (isBankStatementDoc) {
-    if (isMissingByPaths(ocrUiResults, ["documentId", "document_id"])) {
-      missing.push("documentId");
-    }
     if (isMissingByPaths(ocrUiResults, ["bankName", "bank_name"])) {
       missing.push("bankName");
     }
